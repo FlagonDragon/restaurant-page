903cd0a131ae763d497117a2f7e0666374ca1a1a
FlagonDragon <195494654+FlagonDragon@users.noreply.github.com>
2025-09-13 04:48:37 -0300

    homepage on html

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 import './styles.css';
+import pastries from './pastries.png'
 
 console.log('yeah we dooby workin tbh');
+console.log('yooo was goin on');
+
+const pastriesImg = document.createElement('img');
+
+pastriesImg.src = pastries;
+pastriesImg.classList.add("pastriesImg");
+
+
+document.body.appendChild(pastriesImg);
